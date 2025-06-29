499f97b8f55ce1c03712e874777d09bb91a17f06
omkar-s8203 <omkar876742@gmail.com>
2025-06-29 19:50:30 +0530

    feat: Add Vercel Speed Insights integration for performance monitoring

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Analytics } from '@vercel/analytics/react';
 // import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import Index from "./pages/Index";
 import Blog from "./pages/Blog";
 import NotFound from "./pages/NotFound";
@@ -51,7 +52,7 @@ const App = () => (
 
     {/* ⬇️ Render these completely outside providers */}
     <Analytics />
-    {/* <SpeedInsights /> */}
+    <SpeedInsights />
   </>
 );
 
