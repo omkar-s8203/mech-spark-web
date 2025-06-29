7a349a1cf77e55b3bc7619d8af42a4cda06eda28
omkar-s8203 <omkar876742@gmail.com>
2025-06-29 19:30:28 +0530

    feat: Add Vercel Analytics integration to track application performance

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Blog from "./pages/Blog";
 import NotFound from "./pages/NotFound";
+import { Analytics } from '@vercel/analytics/react';
 
 const queryClient = new QueryClient();
 
