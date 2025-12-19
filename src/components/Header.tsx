e42a5107a56c4e2626b09d21ce1d763feaa94ba8
omkar-s8203 <omkar876742@gmail.com>
2025-12-19 21:36:14 +0530

    Update Header component to rename 'Courses' link to 'Product'

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -40,7 +40,7 @@ const Header = () => {
                 Services
               </a>
               <a href="/#courses" className="text-gray-500 hover:text-orange-500 px-3 py-2 text-sm font-medium">
-                Courses
+                Product
               </a>
               <a href="/#about" className="text-gray-500 hover:text-orange-500 px-3 py-2 text-sm font-medium">
                 About
@@ -92,7 +92,7 @@ const Header = () => {
                 Services
               </a>
               <a href="/#courses" className="text-gray-500 hover:text-orange-500 block px-3 py-2 text-base font-medium">
-                Courses
+                Product
               </a>
               <a href="/#about" className="text-gray-500 hover:text-orange-500 block px-3 py-2 text-base font-medium">
                 About
